5c5b6a293aee895a19d29bc51d3c65a833ad6cde
Trakuo <trakuo@gmail.com>
2017-12-03 21:22:46 +0100

    segundo dia

--- openshift/scripts/run-in-container.sh
+++ openshift/scripts/run-in-container.sh
@@ -33,9 +33,7 @@
 
 
 # Get name of a currently deployed pod by label and index
-POD_INSTANCE_NAME=`oc get pods \
-  -l "name=${POD_NAME:-django-frontend}" \
-  -t "{{ with index .items ${POD_INDEX:-0} }}{{ .metadata.name }}{{ end }}"`
+POD_INSTANCE_NAME=`/home/seigpe/proyectos/coci/oc get pods -l "name=${POD_NAME:-django-frontend}"  -t "{{ with index .items ${POD_INDEX:-0} }}{{ .metadata.name }}{{ end }}"`
 
 # Run command in a container of the specified pod:
-oc exec -p "$POD_INSTANCE_NAME" -it -- bash -c "${@:-echo}"
+/home/seigpe/proyectos/coci/oc exec -p "$POD_INSTANCE_NAME" -it -- bash -c "${@:-echo}"
